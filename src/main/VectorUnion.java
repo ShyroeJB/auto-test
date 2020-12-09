909b7940885fb5c57bd2aa78686643bcaa13f13d
Jean-Baptiste Da Costa <jbdacosta.tech@gmail.com>
2020-12-09 11:25:07 +0100

    add javadoc and spec

--- src/main/VectorUnion.java
+++ src/main/VectorUnion.java
@@ -5,6 +5,12 @@ import java.util.Set;
 import java.util.Vector;
 
 public class VectorUnion {
+	/**
+	 * Make a merge of two Vector
+	 * @param a First Vector
+	 * @param b Second Vector
+	 * @return The merging Vector with data a and after data b
+	 */
 	 public static Vector<Object> unionSet(Vector<Object> a, Vector<Object> b) {
 	    Vector<Object> union = new Vector<Object>();
 	    Set<Object> unique = new LinkedHashSet<Object>();
--- src/test/VectorUnionTest.java
+++ src/test/VectorUnionTest.java
@@ -7,7 +7,17 @@ import org.junit.Test;
 
 import main.VectorUnion;
 
+/**
+ * Spec  <br/>
+ * Distinct data, don't have duplication  <br/>
+ * Keep order of data, first vector and after second vector  <br/>
+ * Accept different types in union vector
+ */
 public class VectorUnionTest {
+	/**
+	 * When we have two vector with same data and same data type, we don't accept the duplication
+	 * We must have a vector result with unique/distinct data
+	 */
 	@Test
     public void testDistinctDataVector(){
         Vector<Object> vectorA = new Vector<Object>() {
@@ -37,6 +47,9 @@ public class VectorUnionTest {
         assertEquals(vectorExpected, unionVector);
     }
 
+	/**
+	 * When we merge two vector the final vector must have data with the first data vector in first place and after the second vector data
+	 */
 	@Test
     public void testKeepOrderDataWithSameTypeVector(){
         Vector<Object> vectorA = new Vector<Object>() {
@@ -67,6 +80,10 @@ public class VectorUnionTest {
         assertEquals(vectorExpected, unionVector);
     }
 	
+	/**
+	 * We accept a vector with different types of object
+	 * But we also keep the order, first vector data first and after the second vector 
+	 */
 	@Test
     public void testKeepDifferentTypeDataAndKeepOrderVector(){
         Vector<Object> vectorA = new Vector<Object>() {
